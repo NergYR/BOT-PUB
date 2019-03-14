bb31a120b68f8c25f39d983db91e5802e7ce1ef1
NergYR <46401261+NergYR@users.noreply.github.com>
2019-03-14 20:50:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const cfg require('./index.json'); // a garder en version desktop
 const token = process.env.token // a garder en version heroku
-const prefix = ("?");
+const prefix = ("!");
 
 bot.on('ready', function () {
     console.log("Je suis prêt à être utilisé.")
@@ -40,5 +40,5 @@ bot.on('message', msg => {
 
 });
 
-bot.login(cfg.token); //a garder en version desktop
+bot.login(token); //a garder en version desktop
 bot.login(token); //a garder en version heroku
